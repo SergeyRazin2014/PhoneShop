77a36c90cfdf808f079af575f10bd55fdc54f87e
Sergey Razin <900556@mail.ru>
2019-09-12 16:21:46 +0400

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import './main.css'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createBrowserHistory } from 'history';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
+
+import createRootReducer from 'reducers'
+import routes from 'routes';
+
+const history = createBrowserHistory();
+const middlewares = [thunk, routerMiddleware(history)];
+
+const store = createStore(
+    createRootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares)),
+);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            {routes}
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+)
